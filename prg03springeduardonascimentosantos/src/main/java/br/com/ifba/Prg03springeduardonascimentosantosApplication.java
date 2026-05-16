8d3f75ab6b78004795da243aae66aa2e7ef9d8bc
Edu1212341 <167384147+Edu1212341@users.noreply.github.com>
2026-05-16 19:47:10 -0300

    feat: somente a criação com spring inicializr

---
+++ prg03springeduardonascimentosantos/src/main/java/br/com/ifba/Prg03springeduardonascimentosantosApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prg03springeduardonascimentosantosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prg03springeduardonascimentosantosApplication.class, args);
+	}
+
+}
